1efa739e0486e4c625332a4f446291c343e623d9
Jon Mendoza <jonmendoza1824@gmail.com>
2023-03-11 22:16:47 -0500

    adds '/' home route and attaches handler list

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-
+const dishController = require("./dishes.controller")
 // TODO: Implement the /dishes routes needed to make the tests pass
+router.route("/").get(dishController.list)
 
 module.exports = router;
