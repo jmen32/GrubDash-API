85a53aa2ec49ace4ff4378030f003ef8e49e0b45
Jon Mendoza <jonmendoza1824@gmail.com>
2023-03-12 05:53:48 -0400

    adds create(), dishIdExists(), dishBodyIdExists(), read() handlers and middleware

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -8,12 +8,35 @@ const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
 
+// displays list of dishes
 function list(req, res, next){
-  const { dishId } = req.params;
-  res.json({data: dishes.filter(dishId ? dishes => dishes.id === dishId : () => true)
-})}
+    const { dishId } = req.params;
+    res.json({ data: dishes.filter((dish) => !dishId || dish.id === dishId) });
+}
 
-function hasBodyProperties(req, res, next){
+// Checks to see if the body of the dish contains all needed properties
+function validateBody(req, res, next){
+    const { data: {name, description, price, image_url} = {}} = req.body
+    if (!name  || name === ""){
+        res.status(400).json({error: "Dish must include a name"})
+    }
+    if(!description || description === ""){
+        res.status(400).json({error: "Dish must include a description"})
+    }
+    if(!price){
+        res.status(400).json({error: "Dish must include a price"})
+    }
+    if(price <= 0 || !Number.isInteger(price)){
+        res.status(400).json({error: "Dish must have a price that is an integer greater than 0"})
+    }
+    if(!image_url || image_url === ""){
+        res.status(400).json({error: "Dish must include a image_url"})
+    }
+    next();
+}
+
+// creates new dish and adds to list
+function create(req, res, next){
     const { data: {name, description, price, image_url} = {}} = req.body;
     const newDish = {
         id: nextId(),
@@ -26,14 +49,42 @@ function hasBodyProperties(req, res, next){
     res.status(201).json({data: newDish})
 }
 
-// function read(req, res, next){
-//     const {dishes} = req.params;
-//     const newDish = {
-//         id: 
-//     }
-// }
+// check to see if a dish exists
+function dishIdExists(req, res, next){
+    const {dishId} = req.params;
+    const foundDish = dishes.find((dish) => dish.id === dishId);
+    if(foundDish){
+        res.locals.dish = foundDish;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `Dish does not exist: ${dishId}`
+    })
+}
+
+// check if dishId matches route id
+function dishBodyIdExists(req, res, next){
+    const { data: { id } = {} } = req.body;
+    const {dishId} = req.params;
+    if(id && id === dishId){
+        next();
+    }
+    next({
+        status: 404,
+        message: `Dish id does not match router Id. Dish: ${id}, Route: ${dishId}`
+    })
+}
+
+
+
+function read(req, res, next){
+    res.json({data: res.locals.dish})
+}
 
 
 module.exports = {
     list,
+    create: [validateBody, create],
+    read: [dishIdExists, read],
 }
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const dishController = require("./dishes.controller")
+const methodNotAllowed = require("../errors/methodNotAllowed")
 // TODO: Implement the /dishes routes needed to make the tests pass
-router.route("/").get(dishController.list)
+
+router.route("/:dishId").get(dishController.read).all(methodNotAllowed)
+
+router.route("/").get(dishController.list).post(dishController.create).all(methodNotAllowed)
 
 module.exports = router;
