818fc06dc061ec130f577f77fbc4e724d2658861
Jon Mendoza <jonmendoza1824@gmail.com>
2023-03-11 22:15:29 -0500

    adds middleware to list()

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -2,8 +2,38 @@ const path = require("path");
 
 // Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
-
+// const orders = require("../data/orders-data")
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+function list(req, res, next){
+  const { dishId } = req.params;
+  res.json({data: dishes.filter(dishId ? dishes => dishes.id === dishId : () => true)
+})}
+
+function hasBodyProperties(req, res, next){
+    const { data: {name, description, price, image_url} = {}} = req.body;
+    const newDish = {
+        id: nextId(),
+        name,
+        description,
+        price,
+        image_url,
+    }
+    dishes.push(newDish);
+    res.status(201).json({data: newDish})
+}
+
+// function read(req, res, next){
+//     const {dishes} = req.params;
+//     const newDish = {
+//         id: 
+//     }
+// }
+
+
+module.exports = {
+    list,
+}
