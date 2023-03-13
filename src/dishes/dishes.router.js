1fb798f7687842d9a9e9c78f441d27a747243a53
Jon Mendoza <jonmendoza1824@gmail.com>
2023-03-13 03:13:48 -0400

    adds update router

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -3,7 +3,7 @@ const dishController = require("./dishes.controller")
 const methodNotAllowed = require("../errors/methodNotAllowed")
 // TODO: Implement the /dishes routes needed to make the tests pass
 
-router.route("/:dishId").get(dishController.read).all(methodNotAllowed)
+router.route("/:dishId").get(dishController.read).put(dishController.update).all(methodNotAllowed)
 
 router.route("/").get(dishController.list).post(dishController.create).all(methodNotAllowed)
 
