198d20f95dfadc71e52b0bcf927c62332dd1ec92
Jon Mendoza <jonmendoza1824@gmail.com>
2023-03-14 03:38:30 -0400

    adds methodNotAllowed to '/:dishId' route

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -3,7 +3,7 @@ const ordersController = require("./orders.controller")
 // TODO: Implement the /orders routes needed to make the tests pass
 const methodNotAllowed = require("../errors/methodNotAllowed")
 
-router.route("/:orderId").get(ordersController.readOrders).put(ordersController.updateOrders).delete(ordersController.deleteOrders)
+router.route("/:orderId").get(ordersController.readOrders).put(ordersController.updateOrders).delete(ordersController.deleteOrders).all(methodNotAllowed)
 
 router.route("/").get(ordersController.listOrders).post(ordersController.createOrders).all(methodNotAllowed)
 
